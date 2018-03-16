582ee36c1205ab95a34e1e3163e228c8de9d5008
Alejandro Garcia <argarcia6@dons.usfca.edu>
2018-03-16 00:42:02 -0700

    Add files via upload
    A small program to count how many times a word appears in a text file

---
+++ word_count.py
@@ -0,0 +1,30 @@
+import pprint
+a=open("Alice.txt","r")# text file which is read
+line=" "
+dic={}
+
+while line!="":
+	line=a.readline()
+	split=line.split()
+	
+	for word in split:
+		
+		if word in dic:
+			freq=dic[word]
+			freq=freq+1
+			dic[word]=freq
+		else:
+			dic[word]=1
+	
+pprint.pprint(dic)
+
+
+
+s=input("What word your looking for? ")
+
+if s in dic:
+	print("This word shows up in the file",dic[s],"times")
+if s not in dic:
+	print("Not in the file")
+
+a.close()
